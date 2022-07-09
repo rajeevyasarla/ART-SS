d09d1f465c39a410224d101bf82bc7434ec1d2af
Rajeev Yasarla <rajeevyasarla19@gmail.com>
2022-07-09 11:04:04 -0700

    Add files via upload

--- utils.py
+++ utils.py
@@ -6,31 +6,40 @@ import torch
 import torch.nn.functional as F
 import torchvision.utils as utils
 from math import log10
-from skimage import measure
+from skimage import metrics
+import pdb
+import cv2
 
+def calc_psnr(im1, im2):
+   
+   im1_y = cv2.cvtColor(im1, cv2.COLOR_RGB2YCR_CB)[:, :, 0]
+   im2_y = cv2.cvtColor(im2, cv2.COLOR_RGB2YCR_CB)[:, :, 0]
+   ans = metrics.peak_signal_noise_ratio(im1_y, im2_y)
+   #ans = measure.compare_psnr(im1, im2)
+   #pdb.set_trace()
+   return ans
 
-def to_psnr(pred_image, gt):
-    mse = F.mse_loss(pred_image, gt, reduction='none')
-    mse_split = torch.split(mse, 1, dim=0)
-    mse_list = [torch.mean(torch.squeeze(mse_split[ind])).item() for ind in range(len(mse_split))]
+def calc_ssim(im1, im2):
+   
+   # pdb.set_trace()
 
-    intensity_max = 1.0
-    psnr_list = [10.0 * log10(intensity_max / mse) for mse in mse_list]
-    return psnr_list
+   im1_y = cv2.cvtColor(im1, cv2.COLOR_RGB2YCR_CB)[:, :, 0]
+   im2_y = cv2.cvtColor(im2, cv2.COLOR_RGB2YCR_CB)[:, :, 0]
+   ans = metrics.structural_similarity(im1_y, im2_y)
+   #ans = metrics.structural_similarity(im1, im2, multichannel=True)
+   return ans
 
-
-def to_ssim_skimage(pred_image, gt):
+def to_metrics(pred_image, gt):
     pred_image_list = torch.split(pred_image, 1, dim=0)
     gt_list = torch.split(gt, 1, dim=0)
 
     pred_image_list_np = [pred_image_list[ind].permute(0, 2, 3, 1).data.cpu().numpy().squeeze() for ind in range(len(pred_image_list))]
     gt_list_np = [gt_list[ind].permute(0, 2, 3, 1).data.cpu().numpy().squeeze() for ind in range(len(pred_image_list))]
-    ssim_list = [measure.compare_ssim(pred_image_list_np[ind],  gt_list_np[ind], data_range=1, multichannel=True) for ind in range(len(pred_image_list))]
+    psnr_list = [calc_psnr(pred_image_list_np[ind],  gt_list_np[ind]) for ind in range(len(pred_image_list))]
+    ssim_list = [calc_ssim(pred_image_list_np[ind],  gt_list_np[ind]) for ind in range(len(pred_image_list))]
 
-    return ssim_list
+    return psnr_list,ssim_list
 
-def to_metrics(pred_image, gt):
-    return to_psnr(pred_image, gt),to_ssim_skimage(pred_image, gt)
 
 def validation(net, val_data_loader, device, category, exp_name, save_tag=False):
     """
@@ -50,14 +59,16 @@ def validation(net, val_data_loader, device, category, exp_name, save_tag=False)
             input_im, gt, image_name = val_data
             input_im = input_im.to(device)
             gt = gt.to(device)
-            pred_image,zy_in = net(input_im)
+            #print(input_im.shape)
+            pred_out = net(input_im)
+            pred_image,zy_in = pred_out[0],pred_out[-1]
 
+        im_psnr,im_ssim = to_metrics(pred_image, gt)
         # --- Calculate the average PSNR --- #
-        psnr_list.extend(to_psnr(pred_image, gt))
+        psnr_list.extend(im_psnr)
 
         # --- Calculate the average SSIM --- #
-        ssim_list.extend(to_ssim_skimage(pred_image, gt))
-        #print(image_name,psnr_list[-1],ssim_list[-1])
+        ssim_list.extend(im_ssim)
 
         # --- Save image --- #
         if save_tag:
@@ -92,7 +103,7 @@ def print_log(epoch, num_epochs, one_epoch_time, train_psnr, val_psnr, val_ssim,
 def adjust_learning_rate(optimizer, epoch, category, lr_decay=0.5):
 
     # --- Decay learning rate --- #
-    step = 100 if category == 'derain' else 2
+    step = 40 if category == 'derain' else 2
 
     if not epoch % step and epoch > 0:
         for param_group in optimizer.param_groups:
